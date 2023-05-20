6d1b3347a4e0d16d477d15b4977e7806617c82af
Quiji18 <132489221+Quiji18@users.noreply.github.com>
2023-05-20 12:31:20 -0600

    Add files via upload

--- ProLog.cpp
+++ ProLog.cpp
@@ -4,34 +4,48 @@ using namespace std;
 using namespace funPro;
 int main(){
     string props;
-    int fil, col, prop, pmid, tab[32][50], p=0, q=1, r=2, s=3, op, a, b, c, d, cont=1, x, y, z;
+    int fil, col, prop, pmid, tab[32][50], p=0, q=1, r=2, s=3, op, a, b, c, cont=1, x, y, z, resv=0;
+    do{
+    system("cls");
     cout<<"Programa que genera tablas de verdad de 4 proposiciones "<<endl;
     cout<<"Introdusca cuantas proposiciones simples desea utilizar"<<endl;
     cin>>prop;
     system("cls");
 
     //Inicio Valores Segun Propociciones
-    if(prop == 1){
+    switch (prop)
+    {
+    case 1:
         col = 0;
         fil = 1;
         pmid = 0;
         props = "p";
-    }else if(prop == 2){
+        break;
+    case 2:
         col = 1;
         fil = 3;
         pmid = 1;
         props = "p q";
-    }else if(prop == 3){
+        break;
+    case 3:
         col = 2;
         fil = 7;
         pmid = 3;
         props = "p q r";
-    }else{
+        break;
+    case 4:
         col = 3;
         fil = 15;
         pmid = 7;
         props = "p q r s";
+        break;
+    default:
+        cout<<"Opcion ingresada no valida..."<<endl;
+        system("pause");
+        break;
     }
+    }while (prop<1||prop>4);
+    
     //Final Valores Segun Propociciones
 
     //Inicio Llenar Tabla p
@@ -73,35 +87,47 @@ int main(){
         }
     }system("cls");
     //Final Llenar Tabla s
+    
+    //Inicio Ingresando primera parte de la propocicion compuesta
     cout<<"Ingrese que proposicion desea ingresar"<<endl;
     cout<<"Se le dara las opciones con un ejemplo"<<endl;
-    cout<<"1. Proposicion simple... p"<<endl<<"2. Dos propociciones... pVq"<<endl<<"3. Dos grupos de dos proposiciones... (pVq)^(qVp)"<<endl;
+    cout<<"1. Proposicion simple... p"<<endl<<"2. Dos propociciones... pVq"<<endl;
     cin>>op;
     switch (op){
     case 1:
-        cout<<"Ingrese la propocicion que desea evaluar:";
-        cout<<props<<":"<<endl;
+        valprop(prop);
         cin>>a;
+        c=a;
         break;
     case 2:
-        cout<<"Ingrese las propociccines que desea evaluar: "<<endl;
-        cout<<props<<":"<<endl;
+        valprop(prop);
         cin>>a>>b;
+        valcon();
+        cout<<"Ingrese el conector logico: "<<endl;
+        cin>>x;
+        resv++;
+        for(int i=0;i<=fil;i++){
+            if(prop2(tab[i][a], tab[i][b], x)==true){
+                tab[i][col+resv]=1;
+            }else{
+                tab[i][col+resv]=0;
+            }
+        }
         break;
     default:
         cout<<"Opcion ingresada no valida..."<<endl;
         break;
     }
+    //Final Ingresando primera parte de la propocicion compuesta
 
     //Inicio Imprimir Tabla
+    cout<<props<<endl;
         for(int i=0;i<=fil;i++){
-		for(int j=0;j<=col;j++){
+		for(int j=0;j<=col+resv;j++){
 			cout<<tab[i][j]<<" ";
 		}
         cout<<endl;
         }
     //Fin imprimir Tabla
     return 0;
-
-    
 }
--- funProyect.h
+++ funProyect.h
@@ -1,3 +1,5 @@
+#include <iostream>
+using namespace std;
 namespace funPro{
 
 bool pq(int q, int prop){
@@ -25,23 +27,57 @@ bool pr(int r, int prop){
 bool prop2(int a, int b, int x){
     bool val = false;
     switch (x){
-    case 9:
+    case 8:
         if(a==0&&b==0){
             val=false;
         }else{
             val=true;
         }
         break;
-    case 10:
+    case 9:
         if(a==1&&b==1){
-            val=;
-        }else{
-
+            val=true;
         }
         break;
-    default:
+    case 10:
+        if(a==1&&b==0){
+            val=false;
+        }else{
+            val=true;
+        }
+        break;
+    case 11:
+        if(a==b){
+            val=true;
+        }
         break;
     }
     return val;
+}
+
+void valprop(int propociciones){
+    switch (propociciones){
+    case 1:
+        cout<<endl<<"Valores de proposiciones simples a evaluar"<<endl<<"Ingrese sus valores numericos"<<endl;
+        cout<<" p = 0    ~p = 4"<<endl;
+        break;
+    case 2:
+        cout<<endl<<"Valores de proposiciones simples a evaluar"<<endl<<"Ingrese sus valores numericos"<<endl;
+        cout<<" p = 0     q = 1    ~p = 4    ~q = 5"<<endl;
+        break;
+    case 3:
+        cout<<endl<<"Valores de proposiciones simples a evaluar"<<endl<<"Ingrese sus valores numericos"<<endl;
+        cout<<" p = 0     q = 1     r = 2"<<endl<<"~p = 4    ~q = 5    ~r = 6"<<endl;
+        break;
+    case 4:
+        cout<<endl<<"Valores de proposiciones simples a evaluar"<<endl<<"Ingrese sus valores numericos"<<endl;
+        cout<<" p = 0     q = 1     r = 2     s = 3"<<endl<<"~p = 4    ~q = 5    ~r = 6    ~s = 7"<<endl;
+        break;
+    }
+}
+
+void valcon(){
+    cout<<endl<<"Valores de conectores logicos"<<endl<<"Ingrese sus valores numericos"<<endl;
+    cout<<"V = 8     ^ = 9     -> = 10     <-> = 11"<<endl;
 }
 }
